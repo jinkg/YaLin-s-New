24bc350a071216c0f76bc67a7722d72fd3debdf4
jinkg <349218364@qq.com>
2016-03-02 07:46:18 +0800

    update

--- app/src/androidTest/java/com/yalin/fidouafasm/ApplicationTest.java
+++ app/src/androidTest/java/com/yalin/fidouafasm/ApplicationTest.java
@@ -3,6 +3,8 @@ package com.yalin.fidouafasm;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.yalin.fidouafasm.constants.Constants;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
